4a85647a2f79ba5db29d92bbd38c0d84fda784c6
Julius Farrington <survue@outlook.com>
2015-11-03 20:06:45 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+    @homeland = 'Atlanta'
+    @countries = ['Australia', 'Finland', 'Egypt']
+	@country_pictures = ['australia1.jpg', 'finland1.jpg', 'egypt1.jpg']
+  end
+
+   def about
+   @color = params[:color]
+   @size = params[:size].to_f
+ end
+
+end
